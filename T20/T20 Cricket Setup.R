3511f69c7bf73945babdb3576ccad82749fab888
Jake Nichols <nichols.jacob@gmail.com>
2019-04-06 22:41:40 -0400

    Batting stats, PCA

--- T20/T20 Cricket Setup.R
+++ T20/T20 Cricket Setup.R
@@ -68,7 +68,7 @@ data <- balls %>%
   
   ungroup() %>%
   
-  select(-first) %>%
+  rename(first_ball = first) %>%
   
   mutate(w = ifelse(is.na(wicket_who), NA, ball_no)) %>%
   
@@ -110,7 +110,11 @@ data <- balls %>%
   
   mutate(oppt_total = ifelse(away == batting_team, home_total, away_total)) %>%
   
-  ungroup()
+  ungroup() %>%
+  
+  arrange(match_id, batting_team, ball_id) %>%
+  
+  mutate(after_no_ball = ifelse(lag(play_type) == "no ball", TRUE, FALSE))
   
 
 #
@@ -119,6 +123,49 @@ data <- balls %>%
 
 batter_stats <- data %>%
   
-  group_by(batsman)
+  mutate(batted_runs = ifelse(play_type %in% c("wide", "no ball", "leg bye"), NA, score_value),
+         batted_balls = ifelse(is.na(batted_runs), 0, 1),
+         batter_order = ifelse(is.na(first_ball), NA, batter_order),
+         field_out = ifelse(is.na(wicket_how), NA, 
+                           ifelse(wicket_how %in% c("caught"), 1,
+                                  ifelse(wicket_how %in% c("retired not out (hurt)", "not out"), NA, 0)))
+         ) %>%
   
+  group_by(batsman) %>%
   
+  summarize(runs = sum(batted_runs, na.rm = T),
+            balls = sum(batted_balls, na.rm = T),
+            sixes = sum(score_value == 6, na.rm = T),
+            boundaries = sum(score_value %in% c(4,6), na.rm = T),
+            dismissals = sum(w, na.rm = T),
+            order_appearance = mean(batter_order, na.rm = T),
+            singles = sum(batted_runs == 1, na.rm = T),
+            field_outs = mean(field_out, na.rm = T),
+            powerplays = mean(powerplay, na.rm = T)) %>%
+  
+  ungroup() %>%
+  
+  mutate(runs_ball = runs / balls,
+         runs_over = runs_ball * 6,
+         wickets_ball = dismissals / balls,
+         singles_ball = singles / balls,
+         boundaries_ball = boundaries / balls,
+         sixes_ball = sixes / balls,
+         balls_match = 1 / wickets_ball) %>%
+  
+  select(batsman, runs, balls, sixes, boundaries, dismissals, runs_ball, balls_match, 
+         order_appearance, powerplays, runs_over, wickets_ball, field_outs, boundaries_ball, sixes_ball, singles_ball) %>%
+  
+  filter(balls > 499)
+
+#
+
+km <- kmeans(batter_stats[, 9:15], centers = 5)
+
+pca <- prcomp(batter_stats[, 9:15], scale = TRUE)
+  
+pcad <- cbind(batter_stats, pca$x)
+
+pca$rotation
+
+summary(pca)
